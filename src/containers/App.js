7f856c817acacab80a0057c6855bd8fa9d07f8ef
Yury Burko <yurik_burko@tut.by>
2018-05-11 00:04:52 +0300

    initial commit

---
+++ src/containers/App.js
@@ -0,0 +1,6 @@
+import React, { Component, PropTypes } from 'react'
+export default class App extends Component {
+    render() {
+        return <div>Hello from App</div>
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+    mode: 'development',
+    devtool: 'cheap-module-eval-source-map',
+    entry: [
+        'webpack-hot-middleware/client',
+        'babel-polyfill',
+        './src/index'
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/static/'
+    },
+    plugins: [
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/, 
+                include: [
+                    path.resolve(__dirname, "src"),
+                ],
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        plugins: [ 'transform-runtime', 'react-hot-loader/babel' ]
+                    }
+
+                }]
+            }
+        ]
+    }
+}
